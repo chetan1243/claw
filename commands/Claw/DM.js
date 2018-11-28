df5df34041e8416189ce18f883d528fd0ec17256
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-28 21:59:27 +0530

    Update DM.js

--- commands/Claw/DM.js
+++ commands/Claw/DM.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const botconfig = require("../../botconfig.json");
 module.exports.run = async (bot, message, args) => {
-if(message.author.id !== botconfig.owner) return;
   let user = message.mentions.members.first();
     let msg = args.slice(1).join(" ");
     if(!user) return message.reply("Mention A User To send a Message");
